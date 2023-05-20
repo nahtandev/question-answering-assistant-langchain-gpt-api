0be3fa16927b1b16eedada320ac7f55c6cace0f5
Nathan Gnankadja <gnankadjanathan@gmail.Com>
2023-05-20 15:49:05 +0100

    Create a helper to parse a thread data before send response to user

--- src/helpers/thread.helper.js
+++ src/helpers/thread.helper.js
@@ -8,6 +8,26 @@
 
 const generateSessionID = () => Math.random().toString(16).slice(2);
 
+const formatThreadData = (data) => {
+  let parsedData = {
+    threadId: data.thread_id,
+    threadHistory: data.thread_history,
+    messages: [],
+  };
+
+  // Read message array and format each row
+  data.messages.forEach((message) => {
+    parsedData.messages.push({
+      rang: message.rang,
+      text: message.text,
+      role: message.role,
+    });
+  });
+
+  return parsedData;
+};
+
 module.exports = {
   generateSessionID,
+  formatThreadData,
 };
